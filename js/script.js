1a54d0e1ed5011972dc8b8b696e8380eb179cea9
CarlosDíazGirol <carlosdiazgirol@hotmail.com>
2025-11-05 01:42:34 +0100

    add menu responsive, more products and more

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const ham = document.getElementById("ham")
+const closed = document.getElementById("close-menu")
+const categories = document.getElementById("categories")
+
+ham.addEventListener("click", () => {
+  categories.classList.add("visible-menu")
+})
+
+closed.addEventListener("click", () => {
+  categories.classList.remove("visible-menu")
+})
